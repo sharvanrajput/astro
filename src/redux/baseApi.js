aa0315a57caacc56a15e7205280dfa02dda62f2c
sharvanrajput <sharvanrajput098@gmail.com>
2026-01-16 13:39:26 +0530

    Refactor code structure for improved readability and maintainability

--- src/redux/baseApi.js
+++ src/redux/baseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://adkrayons.com/api",
+    baseURL: "https://astro.adkrayons.com/api",
     headers: {
         "Content-Type": "application/json",
     },
