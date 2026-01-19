da2ed274b69801cf2ef7ef13b2ff5436a7f0d1fc
JYOTI3127 <jyotigautam11312711@gmail.com>
2026-01-19 14:54:57 +0530

    feat: add routing for chat, call, and astrologer details features in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import UpdateUser from './components/Home/UpdateUser'
 import Layout from './layout/Layout'
 import Home from './pages/Home'
 import FreeKundli from './pages/kundli/FreeKundli'
+import ChatWithAstro from './pages/chatwithAstro/ChatWithAstro'
+import CallwithAstro from './pages/callwithAstro/callwithAstro'
+import AstroLogin from './components/AstroRegistration'
+// import Astrodetails from './pages/AstrologerDetails'
+import AstrologerDetails from './pages/AstrologerDetails'
+// import BlogPage from "./pages/BlogPage";
+
 
 const App = () => {
   return (
@@ -14,6 +21,14 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/free-kundli' element={<FreeKundli />} />
           <Route path='/update-user' element={<UpdateUser />} />
+          <Route path='/chat-with-astrologer' element={<ChatWithAstro />} />
+          <Route path='/talk-to-astrologer' element={<CallwithAstro />} />
+          <Route path='/astro-regiter' element={<AstroLogin />} />
+          <Route path='/astro-details' element={<AstrologerDetails />} />
+          {/* <Route path="/blogs" element={<BlogPage />} /> */}
+          {/* <Route path="/blog/:id" element={<BlogDetails />} /> */}
+        
+
         </Route>
       </Routes>
     </>
